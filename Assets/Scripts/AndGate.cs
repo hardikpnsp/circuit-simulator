348bcf5180cf6393d2b9ae883165c2dbf5e10ef7
hardikpnsp <hardikpnsp@gmail.com>
2021-03-14 13:05:21 +0530

    Adds AND gate and OR gate - UI buttons for both - New Sprites for both

---
+++ Assets/Scripts/AndGate.cs
@@ -0,0 +1,67 @@
+using System;
+
+public class AndGate : Movable, IGate
+{
+    Wire inputWireA;
+    Wire inputWireB;
+    Wire outputWire;
+
+    public bool FullyConnected { get; set; }
+    public void CheckFullyConnected()
+    {
+        if (inputWireA != null && inputWireB != null && outputWire != null)
+        {
+            FullyConnected = true;
+        }
+        else
+        {
+            FullyConnected = false;
+        }
+    }
+
+    public void DeRegisterWire(string id)
+    {
+        switch (id)
+        {
+            case "Input_A":
+                inputWireA = null;
+                break;
+            case "Input_B":
+                inputWireB = null;
+                break;
+            case "Output_A":
+                outputWire = null;
+                break;
+            default:
+                throw new Exception("ID not supported");
+        }
+        CheckFullyConnected();
+    }
+
+    public void RegisterWire(string id, Wire wire)
+    {
+        switch (id)
+        {
+            case "Input_A":
+                inputWireA = wire;
+                break;
+            case "Input_B":
+                inputWireB = wire;
+                break;
+            case "Output_A":
+                outputWire = wire;
+                break;
+            default:
+                throw new Exception("ID not supported");
+        }
+        CheckFullyConnected();
+    }
+
+    public void UpdateLogic()
+    {
+        if (FullyConnected)
+        {
+            outputWire.UpdateSignal(inputWireA.signal && inputWireB.signal, this);
+        }
+    }
+}
